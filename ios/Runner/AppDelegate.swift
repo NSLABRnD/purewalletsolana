776268bad0bd8e2c6c0818daae06187675ee14f7
ninano123 <dlwogus1707@naver.com>
2025-04-08 14:12:38 +0900

    I phone build edit

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,8 +1,5 @@
 import UIKit
 import Flutter
-import Firebase
-import flutter_local_notifications
-import app_links
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -10,18 +7,7 @@ import app_links
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    if #available(iOS 10.0, *) {
-        UNUserNotificationCenter.current().delegate = self as UNUserNotificationCenterDelegate
-    }
     GeneratedPluginRegistrant.register(with: self)
-    if #available(iOS 10.0, *) {
-      UNUserNotificationCenter.current().delegate = self as UNUserNotificationCenterDelegate
-    }
-          if let url = AppLinks.shared.getLink(launchOptions: launchOptions) {
-            // We have a link, propagate it to your Flutter app or not
-            AppLinks.shared.handleLink(url: url)
-            return true // Returning true will stop the propagation to other packages
-          }
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
